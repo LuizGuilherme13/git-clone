02d2740bd8abb5afc6d472b42711466fe5ef8d5f
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2024-08-15 23:41:02 -0300

    lista os arquivos nunca adicionados(untracked)

--- cmd/status.go
+++ cmd/status.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io/fs"
+	"log"
+	"path/filepath"
+	"slices"
 
 	"github.com/spf13/cobra"
 )
@@ -15,6 +19,54 @@ var statusCmd = &cobra.Command{
 	Run: status,
 }
 
+var files = []string{}
+
 func status(cmd *cobra.Command, args []string) {
-	fmt.Println("STATUS")
+	err := filepath.Walk(AbsDir, walk)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	index := Index{Path: filepath.Join(AbsDir, ".backup", "index.json")}
+	if err := index.Unmarshal(index.Path); err != nil {
+		log.Fatalln(err)
+	}
+
+	untracked := []string{}
+	objects := []string{}
+
+	for _, obj := range index.Objects {
+		objects = append(objects, obj.Path)
+	}
+
+	for _, file := range files {
+		if contain := slices.Contains(objects, file); !contain {
+			untracked = append(untracked, file)
+		}
+	}
+
+	fmt.Println("Untracked files:")
+	for i := range untracked {
+		fmt.Printf("\t%s\n", untracked[i])
+	}
+
+}
+
+func walk(path string, info fs.FileInfo, err error) error {
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	relPath, err := filepath.Rel(AbsDir, path)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if !info.IsDir() && relPath != "." && !(len(relPath) > 1 && relPath[0] == '.') {
+		files = append(files, relPath)
+	}
+
+	return nil
 }
