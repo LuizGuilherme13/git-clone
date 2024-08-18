e724c95555c06471536cf59d9c4e890a93a49302
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2024-08-17 23:50:52 -0300

    cria o objeto commit e o HEAD

--- cmd/commit.go
+++ cmd/commit.go
@@ -1,7 +1,14 @@
 package cmd
 
 import (
+	"crypto/sha1"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -16,5 +23,67 @@ var commitCmd = &cobra.Command{
 }
 
 func commit(cmd *cobra.Command, args []string) {
-	fmt.Println("COMMIT")
+	commit := Commit{Index: make(map[string]string)}
+	index := Index{Path: filepath.Join(AbsDir, ".backup", "index.json")}
+
+	err := index.Unmarshal(index.Path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, obj := range index.Objects {
+		commit.Index[obj.Path] = obj.Id
+	}
+
+	content, err := os.ReadFile(index.Path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	commit.Hash = fmt.Sprintf("%x", sha1.Sum(content))
+
+	head := filepath.Join(AbsDir, ".backup", "HEAD.txt")
+
+	file, err := os.OpenFile(head, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	parent, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	commit.Parent = string(parent)
+
+	dest := filepath.Join(AbsDir, ".backup/objects", commit.Hash)
+
+	data, err := json.MarshalIndent(commit, "", "  ")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = os.WriteFile(dest, data, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if _, err := os.Stat(head); errors.Is(err, os.ErrNotExist) {
+		_, err := os.Create(head)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	file.Seek(0, io.SeekStart)
+	file.Truncate(0)
+	_, err = file.WriteString(commit.Hash)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+type Commit struct {
+	Hash   string            `json:"hash"`
+	Index  map[string]string `json:"index"`
+	Parent string            `json:"parent"`
 }
