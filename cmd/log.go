fa42beaf392a3fdb5630df207d4514d02419c5cb
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2024-08-27 16:14:37 -0300

    exibe o histórico de commits com o comando `log`

--- cmd/log.go
+++ cmd/log.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -12,9 +16,51 @@ func init() {
 
 var logCmd = &cobra.Command{
 	Use: "log",
-	Run: logF,
+	Run: logFunc,
 }
 
-func logF(cmd *cobra.Command, args []string) {
-	fmt.Println("LOG")
+var pathToCommits = filepath.Join(AbsDir, ".backup", "objects")
+
+func logFunc(cmd *cobra.Command, args []string) {
+	headCommit, err := os.ReadFile(filepath.Join(AbsDir, ".backup", "HEAD.txt"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	content, err := os.ReadFile(filepath.Join(pathToCommits, string(headCommit)))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	commit := Commit{}
+	if err := json.Unmarshal(content, &commit); err != nil {
+		log.Fatalln(err)
+	}
+
+	getParent(pathToCommits, commit)
+}
+
+func getParent(path string, commit Commit) {
+
+	fmt.Printf("commit %s\n", commit.Hash)
+	fmt.Println()
+	fmt.Printf("   %s\n", commit.Message)
+
+	if commit.Parent == "" {
+		fmt.Println("(END)")
+		return
+	}
+	fmt.Println()
+
+	content, err := os.ReadFile(filepath.Join(path, commit.Parent))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	parentCommit := Commit{}
+	if err := json.Unmarshal(content, &parentCommit); err != nil {
+		log.Fatalln(err)
+	}
+
+	getParent(pathToCommits, parentCommit)
 }
